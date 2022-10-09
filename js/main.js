e44b6cbcf857c03cafd67c5f8865f443a54be4b1
francisnpls <111959183+francisnpls@users.noreply.github.com>
2022-10-09 21:54:16 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.auto-type', {
+    strings: ["BSCS Student", "Aspiring Web Developer", "Studying at CVSU - Silang"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 700,
+    loop: true,
+    loopCount: Infinity,
+    autoInsertCss: true
+    })
